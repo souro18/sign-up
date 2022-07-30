cba756b688598b7853a2f51915eeb975049391b5
souro18 <sourajit.paul61@gmail.com>
2022-07-30 19:42:38 +0530

    image update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (<div className="header">
-        <img src="/logo.png" />
+        <img src="/eden-logo.png" alt="eden logo"/>
         <h2>Eden</h2>
     </div>)
 }
